59af7991878c8925e62587ff0f6a7f634b51179f
Sl1nd0 <silindokuhle.nkabinde@apex-rts.co.za>
2019-10-07 07:29:36 +0200

    make it better

--- config/connect.js
+++ config/connect.js
@@ -16,19 +16,16 @@ pool.connect(function(err, res) {
 
 //queryCheck = "CREATE database progresscontrolDB;";
  
-queryCheck = " CREATE TABLE huddleupdates";
-queryCheck += "( huddleid serial NOT NULL, ";
- queryCheck += " huddledate timestamp without time zone NOT NULL, ";
-  queryCheck += " accomplishyesterday text NOT NULL, ";
- queryCheck += "  accomplishtoday text NOT NULL, ";
-queryCheck += "  obstacles character varying(500) NOT NULL, ";
-queryCheck += "  needhelp character varying(400) NOT NULL, ";
-queryCheck += "  userid integer, ";
-queryCheck += "  workid character varying(5), ";
+queryCheck = "CREATE TABLE postlocation ";
+queryCheck += " ( ";
+queryCheck += "  locationid serial NOT NULL, ";
+queryCheck += "    sitename character varying(50) NOT NULL, ";
+queryCheck += "    locationmessage character varying(500) NOT NULL, ";
+ queryCheck += "   locationstatus integer NOT NULL, ";
+ queryCheck += "  userid integer, ";
+queryCheck += "    locationdate timestamp with time zone); ";
+  
 
- queryCheck += "     REFERENCES progressaccount (userid) MATCH SIMPLE ";
-
-queryCheck += " ); ";
  pool.query(
      queryCheck, (err4, qres4) => {
  });
