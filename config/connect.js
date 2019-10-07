21186c3cc0212d666fb6ca2e0af525647046e55f
Sl1nd0 <silindokuhle.nkabinde@apex-rts.co.za>
2019-10-07 07:12:14 +0200

    make it better

--- config/connect.js
+++ config/connect.js
@@ -16,17 +16,17 @@ pool.connect(function(err, res) {
 
 //queryCheck = "CREATE database progresscontrolDB;";
  
-queryCheck = "CREATE TABLE progressaccount( userid serial NOT NULL, username character varying(50) NOT NULL, usersurname character varying(50) NOT NULL, userbirthdate timestamp without time zone NOT NULL, startdate timestamp without time zone NOT NULL, userposition character varying(50) NOT NULL, ";
-  queryCheck += " useremail character varying(255) NOT NULL, ";
- queryCheck += "   usernumber character varying(15) NOT NULL, ";
- queryCheck += "   usergender character varying(20) NOT NULL, ";
- queryCheck += "   userpassword character varying(50) NOT NULL, ";
-  queryCheck += "  useridentity character varying(50) NOT NULL, ";
- queryCheck += "   activestatus character varying(20) DEFAULT 'deactive'::character varying, ";
-  queryCheck += "  workid character varying(5), ";
-  queryCheck += "  sickleave integer,";
-   queryCheck += " annualleave integer";
- queryCheck += "  ); "
+queryCheck = "CREATE TABLE employeeleave ";
+ queryCheck += "( ";
+  queryCheck += " leaveid serial NOT NULL, ";
+ queryCheck += " userid integer, ";
+  queryCheck += " managerid integer, ";
+  queryCheck += " manageremail character varying(100) NOT NULL,";
+ queryCheck += "  leavestatus character varying(50) DEFAULT NULL::character varying, ";
+ queryCheck += "  startdate timestamp without time zone NOT NULL, ";
+queryCheck += "   enddate timestamp without time zone NOT NULL, ";
+  queryCheck += " leavenote character varying(300), ";
+queryCheck += "   leavetype character varying(20) ) ";
  pool.query(
      queryCheck, (err4, qres4) => {
  });
