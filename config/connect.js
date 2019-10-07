7c547be198a2ec7b90b2ce75fc4a664aff48cd84
Sl1nd0 <silindokuhle.nkabinde@apex-rts.co.za>
2019-10-07 07:10:26 +0200

    make it better

--- config/connect.js
+++ config/connect.js
@@ -16,14 +16,17 @@ pool.connect(function(err, res) {
 
 //queryCheck = "CREATE database progresscontrolDB;";
  
-queryCheck = " CREATE TABLE worksite";
-queryCheck += "  ( ";
- queryCheck += " siteid serial NOT NULL, ";
-queryCheck += "  sitename character varying(100), ":
-queryCheck += "  red character varying(10), ";
-queryCheck += "  green character varying(10), ";
-queryCheck += "  blue character varying(10) ";
-queryCheck += " ); ";
+queryCheck = "CREATE TABLE progressaccount( userid serial NOT NULL, username character varying(50) NOT NULL, usersurname character varying(50) NOT NULL, userbirthdate timestamp without time zone NOT NULL, startdate timestamp without time zone NOT NULL, userposition character varying(50) NOT NULL, ";
+  queryCheck += " useremail character varying(255) NOT NULL, ";
+ queryCheck += "   usernumber character varying(15) NOT NULL, ";
+ queryCheck += "   usergender character varying(20) NOT NULL, ";
+ queryCheck += "   userpassword character varying(50) NOT NULL, ";
+  queryCheck += "  useridentity character varying(50) NOT NULL, ";
+ queryCheck += "   activestatus character varying(20) DEFAULT 'deactive'::character varying, ";
+  queryCheck += "  workid character varying(5), ";
+  queryCheck += "  sickleave integer,";
+   queryCheck += " annualleave integer";
+ queryCheck += "  ); "
  pool.query(
      queryCheck, (err4, qres4) => {
  });
