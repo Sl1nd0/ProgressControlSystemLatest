db3e5e9baa46ca732aed304784166ac35335ee7c
Sl1nd0 <silindokuhle.nkabinde@apex-rts.co.za>
2019-10-07 06:56:43 +0200

    make it better

--- config/connect.js
+++ config/connect.js
@@ -14,7 +14,19 @@ pool.connect(function(err, res) {
     }
 });
 
-queryCheck = "CREATE database progresscontrolDB;";
+//queryCheck = "CREATE database progresscontrolDB;";
+ 
+queryCheck = "CREATE TABLE progressaccount( userid serial NOT NULL, username character varying(50) NOT NULL, usersurname character varying(50) NOT NULL, userbirthdate timestamp without time zone NOT NULL, startdate timestamp without time zone NOT NULL, userposition character varying(50) NOT NULL, ";
+  queryCheck += " useremail character varying(255) NOT NULL, ";
+ queryCheck += "   usernumber character varying(15) NOT NULL, ";
+ queryCheck += "   usergender character varying(20) NOT NULL, ";
+ queryCheck += "   userpassword character varying(50) NOT NULL, ";
+  queryCheck += "  useridentity character varying(50) NOT NULL, ";
+ queryCheck += "   activestatus character varying(20) DEFAULT 'deactive'::character varying, ";
+  queryCheck += "  workid character varying(5), ";
+  queryCheck += "  sickleave integer,";
+   queryCheck += " annualleave integer";
+ queryCheck += "  ); ";
  pool.query(
      queryCheck, (err4, qres4) => {
  });
