d1149faaf04821b87b3eb2041859b0da2969df63
unknown <ssankabi@gmail.com>
2019-12-10 15:23:56 +0200

    make it better

--- config/connect2.js
+++ config/connect2.js
@@ -14,7 +14,14 @@ pool.connect(function(err, res) {
     }
 });
 
-queryCheck = "create database progresscontrolDB;";
+//queryCheck = "create database progresscontrolDB;";
+queryCheck = "CREATE TABLE public.progressaccount (";
+
+queryCheck += " userid serial PRIMARY KEY,  username character varying(50) NOT NULL,  usersurname character varying(50) NOT NULL, ";
+ queryCheck += " userbirthdate timestamp without time zone NOT NULL,  startdate timestamp without time zone NOT NULL,  userposition character varying(50) NOT NULL, ";
+  queryCheck += " useremail character varying(255) NOT NULL,  usernumber character varying(20) NOT NULL,  usergender character varying(20) NOT NULL, ";
+ queryCheck += " userpassword character varying(50) NOT NULL,  useridentity character varying(13),  workid character varying(2) NOT NULL,  activestatus character varying(20) DEFAULT 'deactive' )";
+
  
  pool.query(
      queryCheck, (err4, qres4) => {
