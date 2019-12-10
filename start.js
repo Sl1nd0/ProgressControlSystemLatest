3787297ed63cd92a8f894088db05502266ee7b86
unknown <ssankabi@gmail.com>
2019-12-10 14:54:39 +0200

    make it better

--- start.js
+++ start.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var pool = require('../config/connect2');
+var pool = require('./config/connect2');
 
 var app = express();
 var port = process.env.PORT || 4000; 
