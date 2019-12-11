b827b4eb2dc742f3a9c70c073ba101981594f9ad
unknown <ssankabi@gmail.com>
2019-12-11 13:54:19 +0200

    make it better

--- public/js/controllers/logoutController/logoutController.js
+++ public/js/controllers/logoutController/logoutController.js
@@ -1,7 +1,5 @@
 app.controller('logoutController', function ($scope, $rootScope, $window, $sessionStorage, $localStorage, $location,  $http, chatSocket, logoutService) {
 
-	alert('Hi Mr Logout!!');
-	
 	//Logging out
 	logoutService.logoutAccount()
 	.then(function (response)
